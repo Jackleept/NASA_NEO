bb321e44c06b12543567e24dca6996bf7a74cf94
Jackleept <j@cklee.uk>
2023-09-23 08:22:08 +0100

    rename for new main

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import requests
 import datetime
 import pprint
 import sqlite3
+import json
+import pandas as pd
 
 link = "https://api.nasa.gov/neo/rest/v1/feed?start_date=LAST_WEEK&end_date=&api_key=Gt87ibmZefPpnhl8gfz5gWWiTuftebq6IgJBFNdQ"
 
@@ -16,8 +18,7 @@ response = requests.get(link)
 data = response.json()
 neo_data = data["near_earth_objects"]
 
-#pprint.pprint(data)
-#pprint.pprint(neo_data)
+pprint.pprint(neo_data)
 
 conn = sqlite3.connect("NASA_NEO.db")
 cursor = conn.cursor()
@@ -27,33 +28,16 @@ cursor.executescript('''
     DROP TABLE IF EXISTS neo;
 
     CREATE TABLE neo (
-                     id TEXT PRIMARY KEY,
-                     name TEXT,
-                     potentially_hazardous BOOLEAN,
-                     sentry_object BOOLEAN,
-                     close_approach_date TEXT,
-                     miss_distance_km TEXT,
-                     orbiting_body TEXT,
-                     relative_velocity_kps TEXT,
-                     estimated_diameter_max_m TEXT,
-                     estimated_diameter_min_m TEXT
+                     id text,
+                     data json
     )
 ''')
 
 for date, neo_list in neo_data.items():
     for neo in neo_list:
-        cursor.execute('''
-                    INSERT INTO neo VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
-                    ''', (
-                        neo["id"],
-                        neo["name"],
-                        neo["is_potentially_hazardous_asteroid"],
-                        neo["is_sentry_object"],
-                        date,
-                        neo["miss_distance"],
-                        neo["orbiting_body"],
-                        neo["relative_velocity"["kilometres_per_second"]],
-                        neo["estimated_diameter"["metres"["estimated_diameter_max"]]],
-                        neo["estimated_diameter"["metres"["estimated_diameter_min"]]]
-                    ))
+        cursor.execute("INSERT INTO neo VALUES (?, ?)",
+                    (neo["id"], json.dumps(neo)))
 conn.commit()
+
+pd.read_sql_query('''
+                  select json_extract(data)''')
--- test2.py
+++ test2.py
@@ -2,7 +2,6 @@ import requests
 import datetime
 import pprint
 import sqlite3
-import json
 
 link = "https://api.nasa.gov/neo/rest/v1/feed?start_date=LAST_WEEK&end_date=&api_key=Gt87ibmZefPpnhl8gfz5gWWiTuftebq6IgJBFNdQ"
 
@@ -17,35 +16,44 @@ response = requests.get(link)
 data = response.json()
 neo_data = data["near_earth_objects"]
 
+#pprint.pprint(data)
 #pprint.pprint(neo_data)
 
-conn = sqlite3.connect("TEST2.db")
+conn = sqlite3.connect("test2.db")
 cursor = conn.cursor()
 
 cursor.executescript('''
-                     
-            DROP TABLE IF EXISTS neo;
-                     
-            CREATE TABLE IF NOT EXISTS neo (
-            id TEXT PRIMARY KEY,
-            name TEXT,
-            date DATE,
-            is_potentially_hazardous BOOLEAN
-        )
-    ''')
 
-# Iterate through the NEO data and insert it into the database
+    DROP TABLE IF EXISTS neo;
+
+    CREATE TABLE neo (
+                     id TEXT PRIMARY KEY,
+                     name TEXT,
+                     potentially_hazardous BOOLEAN,
+                     sentry_object BOOLEAN,
+                     close_approach_date TEXT,
+                     miss_distance_km TEXT,
+                     orbiting_body TEXT,
+                     relative_velocity_kps TEXT,
+                     estimated_diameter_max_m TEXT,
+                     estimated_diameter_min_m TEXT
+    )
+''')
+
 for date, neo_list in neo_data.items():
     for neo in neo_list:
         cursor.execute('''
-            INSERT OR REPLACE INTO neo (id, name, date, is_potentially_hazardous)
-            VALUES (?, ?, ?, ?)
-        ''', (
-            neo['id'],
-            neo['name'],
-            date,
-            neo['is_potentially_hazardous_asteroid']
-        ))
-
-# Commit the changes and close the database connection
+                    INSERT INTO neo VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+                    ''', (
+                        neo["id"],
+                        neo["name"],
+                        neo["is_potentially_hazardous_asteroid"],
+                        neo["is_sentry_object"],
+                        date,
+                        neo["miss_distance"],
+                        neo["orbiting_body"],
+                        neo["relative_velocity"["kilometres_per_second"]],
+                        neo["estimated_diameter"["metres"["estimated_diameter_max"]]],
+                        neo["estimated_diameter"["metres"["estimated_diameter_min"]]]
+                    ))
 conn.commit()
